8bcef8cf28113f1ad407e72133f322a52c4bb56b
Jose Yapur <jose@yapur.me>
2025-02-17 14:52:29 -0500

    Update base-api.ts

--- frontend/src/base-api.ts
+++ frontend/src/base-api.ts
@@ -1,4 +1,4 @@
 // This file contains the base URL for the API without the trailing slash
-const BASE_API_URL = 'https://6pdr7o57a1.execute-api.us-east-1.amazonaws.com/Prod'
+const BASE_API_URL = 'https://lcnqyurobh.execute-api.us-east-1.amazonaws.com/Prod'
 
 export default BASE_API_URL;
